bf9be095c0cfb636b9b8e3b7df2dde1b75be2e18
Ryan Hu <ryanhuuu@gmail.com>
2018-03-21 22:22:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,14 @@ def long_planeteer_calls(arr)
   }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
+  
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  return arr.any? { |ele|
+    cheese_types.include(ele)
+  }
+  
 end
