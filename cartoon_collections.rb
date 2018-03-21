684e70c7be674a1cdcabeab44e1213894cb639f9
Ryan Hu <ryanhuuu@gmail.com>
2018-03-21 22:20:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ def summon_captain_planet(arr)
   return arr
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  return arr.any? { |ele|
+    ele.length > 4
+  }
 end
 
 def find_the_cheese# code an argument here
